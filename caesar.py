8d17b930996cccb055ff4313254a846425b64c62
unknown <kevinconwaylax@gmail.com>
2023-02-13 15:18:40 -0500

    added replayability functionality

--- caesar.py
+++ caesar.py
@@ -1,13 +1,15 @@
 # Import logo and print for start of game
 from art import logo
-print (logo)
+print(logo)
 
 # Alphabet for iterating through
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # Function to calculate new message
+
+
 def caesar(txt, shft, dir):
     # Empty string for end result
     end_text = ""
@@ -29,22 +31,25 @@ def caesar(txt, shft, dir):
             new_position = position + shft
             # Add letter to the end of the new string created before the for loop
             end_text += alphabet[new_position]
-    
+
     # Print result
     print(f"Here's the {dir}d result: {end_text}\n")
 
 
-# Prompt the user for data entry
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+# Variable for end of game condition
+end = 'yes'
 
-# If the shift number is greater than 26 it will cause an IndexOutOfRange
-# The '%26' makes the dunction work the same way (encoding or decoding )
-if shift > 26:
-    shift %= 26
+while end == 'yes':
+    # Prompt the user for data entry
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-# Calculate new string
-caesar(txt=text, shft=shift, dir=direction)
+    # If the shift number is greater than 26 it will cause an IndexOutOfRange
+    # The '%26' makes the dunction work the same way (encoding or decoding )
+    if shift > 26:
+        shift %= 26
 
-  
+    # Calculate new string
+    caesar(txt=text, shft=shift, dir=direction)
+    end = input("Would you like to restart the program? Type 'yes' to continue.\n")
